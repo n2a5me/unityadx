eebf8e2001bebd6444432e91aff3572026365e51
ngocnm <ngocnm@appota.com>
2018-08-31 14:56:09 +0700

    First commit

---
+++ UnityAdxSample/Assets/GameScript.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class GameScript : MonoBehaviour {
+    private BannerView bannerView;
+    private InterstitialAd interstitial;
+    string AppId = "YOUR_ANDROID_APP_ID";
+    string BannerAdUnitId = "/21617015150/40789737/21740635562";//Replace with your banner ad unit id
+    string InterstitialAdUnitId = "/21617015150/40789737/21740466811";//Replace with your interstitial unit id
+
+    // Use this for initialization
+    void Start () {
+
+        MobileAds.SetiOSAppPauseOnBackground(true);
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(AppId);
+
+        this.InitBanner();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowInterstitial()
+    {
+        if (this.interstitial != null && this.interstitial.IsLoaded())
+        {
+            this.interstitial.Show();
+        }
+        else
+        {
+            MonoBehaviour.print("Interstitial is not ready yet");
+            this.RequestInterstitial();
+        }
+    }
+
+    private AdRequest CreateInterstitialAdRequest()
+    {
+        return new AdRequest.Builder()
+            .AddTestDevice(AdRequest.TestDeviceSimulator)
+            .AddTestDevice("0123456789ABCDEF0123456789ABCDEF")
+            .Build();
+    }
+
+    private void InitBanner()
+    {
+       
+
+        // Create a 320x50 banner at the top of the screen.
+        bannerView = new BannerView(BannerAdUnitId, AdSize.Banner, AdPosition.Bottom);
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        bannerView.LoadAd(request);
+    }
+
+    private void RequestInterstitial()
+    {
+       
+
+        // Clean up interstitial ad before creating a new one.
+        if (this.interstitial != null)
+        {
+            this.interstitial.Destroy();
+        }
+
+        // Create an interstitial.
+        this.interstitial = new InterstitialAd(InterstitialAdUnitId);
+
+        // Register for ad events.
+        this.interstitial.OnAdLoaded += this.HandleInterstitialLoaded;
+        this.interstitial.OnAdFailedToLoad += this.HandleInterstitialFailedToLoad;
+        this.interstitial.OnAdOpening += this.HandleInterstitialOpened;
+        this.interstitial.OnAdClosed += this.HandleInterstitialClosed;
+        this.interstitial.OnAdLeavingApplication += this.HandleInterstitialLeftApplication;
+
+        // Load an interstitial ad.
+        this.interstitial.LoadAd(this.CreateInterstitialAdRequest());
+    }
+
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLoaded event received");
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                            + args.Message);
+    }
+
+    public void HandleInterstitialLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleInterstitialLoaded event received");
+        this.interstitial.Show();
+    }
+
+    public void HandleInterstitialFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print(
+            "HandleInterstitialFailedToLoad event received with message: " + args.Message);
+    }
+
+    public void HandleInterstitialOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleInterstitialOpened event received");
+    }
+
+    public void HandleInterstitialClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleInterstitialClosed event received");
+    }
+
+    public void HandleInterstitialLeftApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleInterstitialLeftApplication event received");
+    }
+}
